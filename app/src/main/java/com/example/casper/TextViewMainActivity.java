0aabffd53af17fa2d38d722a207c586077d8704d
PUREzz <1043549017@qq.com>
2019-09-02 23:29:10 +0800

    Hello_World4

--- app/src/main/java/com/example/casper/TextViewMainActivity.java
+++ app/src/main/java/com/example/casper/TextViewMainActivity.java
@@ -5,13 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class TextViewMainActivity extends AppCompatActivity {
 
     private TextView textViewHelloWorld;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.text_View_activity_main);
 
         textViewHelloWorld = findViewById(R.id.textViewHelloWorld);
         textViewHelloWorld.setText("你好，编程实现的Hello_World");
